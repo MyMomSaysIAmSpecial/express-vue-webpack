f20003a8f9d3180a38288e81d36f685ab89e1d48
Roman Taranenko <roman@aruodas.lt>
2017-03-09 16:42:59 +0200

    Minifying

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,3 +1,8 @@
+'use strict';
+
+var webpack = require('webpack');
+
+process.noDeprecation = true;
 module.exports = {
     // This is the "main" file which should include all other modules
     entry: './src/main.js',
@@ -16,11 +21,24 @@ module.exports = {
                 test: /\.js$/,
                 // Transform it with babel
                 loader: 'babel-loader',
+                options: {
+                    babelrc: false,
+                    presets: [
+                        'env'
+                    ]
+                },
                 // don't transform node_modules folder (which don't need to be compiled)
                 exclude: /(node_modules|bower_components)/
             }
         ]
     },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ],
     resolve: {
         alias: {
             vue: 'vue/dist/vue.js'
